52ebb7aa73fe35469c46a2cbe48a798a64f5012e
shezzy7 <jamshahzad72242@gmail.com>
2024-11-19 19:08:33 +0500

    Backend4-Js-OOP

--- Backend4-Javascript-OOP/Server/index.js
+++ Backend4-Javascript-OOP/Server/index.js
@@ -5,3 +5,10 @@ let port = 8080;
 app.listen(port , ()=>{
     console.log("Listening on Port number : " , port);
 })
+app.get("/register"  , (req , res)=>{
+    let {user} = req.query;
+    res.send(`Standard get method.Welcome ${user}`);
+})
+app.post("/register"  , (req , res)=>{    
+    res.send(`Standard Post method`);
+})
